a5fe17ff4b266d4ed6c05155b84ce42bd41808ec
Manuel Gomes <manuel.fgj@gmail.com>
2022-01-26 11:41:31 -0300

    first and only commit

---
+++ src/components/Botao.tsx
@@ -0,0 +1,19 @@
+interface BotaoProps {
+  cor?: 'green' | 'blue' | 'black'
+  className?: string
+  children: any
+  onClick?: () => void
+}
+
+export default function Botao(props: BotaoProps) {
+  const cor = props.cor ?? 'black'
+  return (
+    <button onClick={props.onClick} className={`
+      bg-gradient-to-r from-${cor}-400 to-${cor}-700
+      text-white px-4 py-2 rounded-md
+      ${props.className}
+      `}>
+      {props.children}
+    </button>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  //content: [],
+  purge: {
+    content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx}",
+      "./src/components/**/*.{js,ts,jsx,tsx}",
+    ],
+    safelist: [
+      /^bg-/,
+      /^to-/,
+      /^from/,
+    ]
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
